b23162638682747bf19b56a875e559febcf71365
omeravhar <37772161+omeravhar@users.noreply.github.com>
2020-04-13 22:03:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.get('avharchat.herokuapp.com',(req,res)=>{
-    res.sendFile(__dirname+'/index.html');
+app.get('/',(req,res)=>{
+    res.sendFile('index.html');
 });
 
 
