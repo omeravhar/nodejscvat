6665753ff884e1e1123a45d6a43ada0ba36ac393
omerav <omeravhar@gmail.com>
2020-04-13 13:19:08 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+
+io.sockets.on('connect',(socket)=>{
+    socket.on('input',(ms)=>{
+        io.sockets.emit('new',ms);
+    });
+});
+
+
+
+
+http.listen(3000,()=>{
+    console.log('server is runnig on port 3000');
+}); 
