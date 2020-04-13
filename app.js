ba41f3cb13e90dceb4f2eab2861b3777db6753be
omeravhar <37772161+omeravhar@users.noreply.github.com>
2020-04-13 22:05:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 app.get('/',(req,res)=>{
-    res.sendFile('index.html');
+    res.sendFile(__dirname+'/index.html');
 });
 
 
